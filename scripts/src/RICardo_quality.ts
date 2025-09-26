ba6519562493019a184b05eef49df5450b96af15
Paul Girard <paul@ouestware.com>
2025-09-26 16:34:57 +0200

    [quality] for gravity #4

--- scripts/src/RICardo_quality.ts
+++ scripts/src/RICardo_quality.ts
@@ -7,15 +7,31 @@ import { DB } from "./DB";
 import { EdgeAttributes, FlowValueImputationMethod, GraphType } from "./types";
 import { getTradeGraphsByYear } from "./utils";
 
+interface FlowDataPoint {
+  year: number;
+  importerId: string;
+  importerLabel: string;
+  importerType: string;
+  exporterId: string;
+  exporterLabel: string;
+  exporterType: string;
+  valueFromImporter?: number;
+  valueFromExporter?: number;
+  status: EdgeAttributes["status"];
+  notes?: string;
+}
+
 interface ComputedData {
   year: number;
   bilaterals: Record<FlowStatType, FlowStat>;
   nbReportingFT: number;
   nbGPHAutonomousCited: number;
+  GPHAutonomousCited: { id: string; label: string }[];
   worldFT: number;
   worldBilateral: number;
   inFTNotInBilateral: string[];
   inBilateralNotInFT: string[];
+  flowData: FlowDataPoint[];
 }
 
 const headers: string[] = [
@@ -39,7 +55,7 @@ const headers: string[] = [
   ),
   "inFTNotInBilateral",
   "inBilateralNotInFT",
-];
+] as const;
 
 // TODO : recode form netwtok data
 // priority exp on imp on mirror : premier alphabétique
@@ -98,14 +114,20 @@ async function graphQuality(graph: GraphType): Promise<ComputedData> {
     ignore_internal: { nbFlows: 0, value: 0 },
     discard_collision: { nbFlows: 0, value: 0 },
     splitFailedParts: { nbFlows: 0, value: 0 },
+    split_only_partial: { nbFlows: 0, value: 0 },
+    split_failed_no_ratio: { nbFlows: 0, value: 0 },
+    split_failed_error: { nbFlows: 0, value: 0 },
   };
   const sumBilateralWorld = { nbFlows: 0, value: 0 };
+  const flowData: FlowDataPoint[] = [];
   graph.edges().forEach((e) => {
     const edgeAtts = graph.getEdgeAttributes(e);
     const value = getEdgeValue(edgeAtts);
     if ((edgeAtts.labels.has("REPORTED_TRADE") || edgeAtts.labels.has("GENERATED_TRADE")) && edgeAtts.status) {
       if (value && edgeAtts.status !== "ignore_resolved") {
         // if generated_trade track the method used for resolution
+        const ok =
+          edgeAtts.status === "ok" && graph.source(e) === "restOfTheWorld" && graph.target(e) === "restOfTheWorld";
         let status: FlowStatType | undefined = edgeAtts.labels.has("GENERATED_TRADE")
           ? edgeAtts.valueGeneratedBy
           : edgeAtts.status;
@@ -115,18 +137,30 @@ async function graphQuality(graph: GraphType): Promise<ComputedData> {
         if (status !== undefined) {
           bilaterals[status].nbFlows += 1;
           bilaterals[status].value += value;
+
           // bilateral sum
-          if (
-            status !== "splitFailedParts" &&
-            status !== "toTreat" &&
-            status !== "ignore_internal" &&
-            status !== "discard_collision"
-          ) {
+          if (ok) {
             sumBilateralWorld.nbFlows += 1;
             sumBilateralWorld.value += value;
           }
         }
       }
+      const importer = graph.getTargetAttributes(e);
+      const exporter = graph.getSourceAttributes(e);
+
+      flowData.push({
+        year,
+        importerId: graph.target(e),
+        importerLabel: importer.label,
+        importerType: importer.type,
+        exporterId: graph.source(e),
+        exporterLabel: exporter.label,
+        exporterType: exporter.type,
+        valueFromExporter: edgeAtts.Exp,
+        valueFromImporter: edgeAtts.Imp,
+        status: edgeAtts.status,
+        notes: edgeAtts.notes,
+      });
     }
   });
   const GPHAutonomousCited = sortBy(
@@ -134,12 +168,13 @@ async function graphQuality(graph: GraphType): Promise<ComputedData> {
       .filterNodes((_, atts) => {
         return atts.type === "entity" && atts.entityType === "GPH-AUTONOMOUS-CITED";
       })
-      .map((id) => graph.getNodeAttribute(id, "label")),
+      .map((id) => ({ id, label: graph.getNodeAttribute(id, "label") })),
   );
   const nbGPHAutonomousCited = GPHAutonomousCited.length;
 
-  const inFTNotInBilateral = difference(reportingsFT, GPHAutonomousCited);
-  const inBilateralNotInFT = difference(GPHAutonomousCited, reportingsFT);
+  const GPHAutonomousCitedLabels = GPHAutonomousCited.map((e) => e.label);
+  const inFTNotInBilateral = difference(reportingsFT, GPHAutonomousCitedLabels);
+  const inBilateralNotInFT = difference(GPHAutonomousCitedLabels, reportingsFT);
 
   return {
     year: graph.getAttribute("year"),
@@ -150,37 +185,87 @@ async function graphQuality(graph: GraphType): Promise<ComputedData> {
     worldBilateral: sumBilateralWorld.value,
     inFTNotInBilateral,
     inBilateralNotInFT,
+    GPHAutonomousCited,
+    flowData,
   };
 }
 
 async function graphsQuality() {
   //TODO do not load all the graphs at once.
   const tradeGraphsByYear = await getTradeGraphsByYear(true);
-  const tasks = values(tradeGraphsByYear).map((graph) => async () => graphQuality(graph));
-  const qualityStats = await parallelize(tasks, 5);
-  const csvString = stringify(
-    qualityStats.map((data) => {
-      const bilateralsStats = toPairs(data.bilaterals).reduce((acc, [key, stats]) => {
-        return { ...acc, [`${key}_flows`]: stats.nbFlows, [`${key}_value`]: stats.value };
-      }, {});
+  // prepare out streams
+  const GPHAutonomousCited: Record<string, { id: string; label: string; years: number[] }> = {};
+  const statsStream = fs.createWriteStream("../data/tradeGraphsStats.csv", { flags: "w" });
+  const flowStream = fs.createWriteStream("../data/tradeFlows.csv", { flags: "w" });
+  let firstLine = true;
 
-      return {
-        year: data.year,
-        nbGPHAutonomousCited: data.nbGPHAutonomousCited,
-        nbReportingFT: data.nbReportingFT,
-        worldBilateral: data.worldBilateral,
-        worldFT: data.worldFT,
-        ...bilateralsStats,
-        inFTNotInBilateral: data.inFTNotInBilateral.join("|"),
-        inBilateralNotInFT: data.inBilateralNotInFT.join("|"),
+  const tasks = values(tradeGraphsByYear).map((graph) => async () => {
+    const qualityStats = await graphQuality(graph);
+    // BilateralStats
+    const bilateralsStats = toPairs(qualityStats.bilaterals).reduce((acc, [key, stats]) => {
+      return { ...acc, [`${key}_flows`]: stats.nbFlows, [`${key}_value`]: stats.value };
+    }, {});
+
+    const stats = {
+      year: qualityStats.year,
+      nbGPHAutonomousCited: qualityStats.nbGPHAutonomousCited,
+      nbReportingFT: qualityStats.nbReportingFT,
+      worldBilateral: qualityStats.worldBilateral,
+      worldFT: qualityStats.worldFT,
+      ...bilateralsStats,
+      inFTNotInBilateral: qualityStats.inFTNotInBilateral.join("|"),
+      inBilateralNotInFT: qualityStats.inBilateralNotInFT.join("|"),
+    };
+    statsStream.write(
+      stringify([stats], {
+        header: firstLine,
+        columns: headers,
+      }),
+    );
+
+    flowStream.write(
+      stringify(qualityStats.flowData, {
+        header: firstLine,
+        columns: [
+          "year",
+          "importerId",
+          "importerLabel",
+          "importerType",
+          "exporterId",
+          "exporterLabel",
+          "exporterType",
+          "valueFromImporter",
+          "valueFromExporter",
+          "status",
+          "notes",
+        ],
+      }),
+    );
+
+    // GPHAutonomousCited list
+    qualityStats.GPHAutonomousCited.forEach((GPH) => {
+      GPHAutonomousCited[GPH.id] = {
+        ...GPH,
+        years: sortBy([...(GPHAutonomousCited[GPH.id] ? GPHAutonomousCited[GPH.id].years : []), qualityStats.year]),
       };
-    }),
-    {
-      header: true,
-      columns: headers,
-    },
+    });
+
+    firstLine = false;
+  });
+  await parallelize(tasks, 5);
+  statsStream.end();
+  flowStream.end();
+
+  fs.writeFileSync(
+    "../data/GPHAutonomousCited.csv",
+    stringify(
+      toPairs(GPHAutonomousCited).reduce<{ GPH_code: string; label: string; years: string }[]>(
+        (acc, [GPH, { label, years }]) => [...acc, { GPH_code: GPH, label, years: years.join("|") }],
+        [],
+      ),
+      { header: true, columns: ["GPH_code", "label", "years"] },
+    ),
   );
-  fs.writeFileSync("../data/tradeGraphsStats.csv", csvString);
 }
 
 graphsQuality().then(() => {
