9cfa35a52d41c623ab1bd40294bda23bba8f92ac
YoussefGld <youssef.ghallada@uclouvain.be>
2026-07-19 16:26:13 +0200

    Update

--- scripts/Ghallada/RegionalTradeintensityYear.R
+++ scripts/Ghallada/RegionalTradeintensityYear.R
@@ -191,3 +191,112 @@ p_combine <- ggplot(combine, aes(year, moyenne, color = methode)) +
 print(p_combine)
 ggsave("data/blocks/tibi_comparaison_intramax_louvain.png",
        plot = p_combine, width = 10, height = 5, dpi = 150)
+
+
+###Regional blocs (as echoed by Anderson and Norheim)
+BlocselonAN <- as.data.frame(read_excel("data/BlocselonAN.xlsx"))
+region_map <- BlocselonAN[, c("GPH_code", "region_AndersonNorheim")]
+region_map$GPH_code <- as.character(region_map$GPH_code)
+
+resultats_AN <- data.frame(year = integer(), moyenne = numeric(), n_blocs = integer())
+
+for (year in annees) {
+  f <- paste0("data/blocks/Intramax/paires_blocs_", year, ".csv")
+  if (!file.exists(f) || file.info(f)$size == 0) next
+  d <- read.csv(f, stringsAsFactors = FALSE)
+  d <- d[!is.na(d$value), ]
+  if (nrow(d) == 0) next
+
+  # ---- merger la region AN par code GPH (exportateur + importateur) ----
+  d$exporterId <- as.character(d$exporterId)
+  d$importerId <- as.character(d$importerId)
+  d <- merge(d, region_map, by.x = "exporterId", by.y = "GPH_code", all.x = TRUE)
+  names(d)[names(d) == "region_AndersonNorheim"] <- "region_exp"
+  d <- merge(d, region_map, by.x = "importerId", by.y = "GPH_code", all.x = TRUE)
+  names(d)[names(d) == "region_AndersonNorheim"] <- "region_imp"
+  
+  d <- d[!is.na(d$region_exp) & !is.na(d$region_imp) &
+           d$region_exp != "" & d$region_imp != "", ]
+  if (nrow(d) == 0) next
+  
+  Xij <- aggregate(value ~ region_exp + region_imp, data = d, FUN = sum)
+  names(Xij) <- c("i", "j", "Xij")
+  blocs <- sort(unique(c(d$region_exp, d$region_imp)))
+  
+  grille <- expand.grid(i = blocs, j = blocs, stringsAsFactors = FALSE)
+  grille <- merge(grille, Xij, by = c("i", "j"), all.x = TRUE)
+  grille$Xij[is.na(grille$Xij)] <- 0
+
+  X_tot <- sum(grille$Xij)
+  Xi <- tapply(grille$Xij, grille$i, sum)
+  Xj <- tapply(grille$Xij, grille$j, sum)
+  grille$Xi <- Xi[as.character(grille$i)]
+  grille$Xj <- Xj[as.character(grille$j)]
+
+  
+  grille$s_ij <- grille$Xij / grille$Xi
+  grille$Xrj  <- grille$Xj - grille$Xij
+  grille$Xr   <- X_tot - grille$Xi
+  grille$s_rj <- grille$Xrj / grille$Xr
+  grille$I1 <- grille$s_ij / grille$s_rj
+  grille$I3 <- (1 - grille$s_ij) / (1 - grille$s_rj)
+  ratio <- grille$I1 / grille$I3
+  grille$TIBI <- (ratio - 1) / (ratio + 1)
+  
+  tibi <- grille[, c("i", "j", "Xij", "TIBI")]
+  
+  
+  exp <- unique(d[, c("exporterId", "region_exp")]); names(exp) <- c("gph", "bloc")
+  imp <- unique(d[, c("importerId", "region_imp")]); names(imp) <- c("gph", "bloc")
+  pays_bloc <- unique(rbind(exp, imp))
+  taille <- table(pays_bloc$bloc)
+  blocs_singletons <- names(taille)[taille == 1]
+  
+  
+  tibi_ss <- tibi[!(tibi$i %in% blocs_singletons) & !(tibi$j %in% blocs_singletons), ]
+  diag_df <- tibi_ss[tibi_ss$i == tibi_ss$j & !is.nan(tibi_ss$TIBI) & is.finite(tibi_ss$TIBI),
+                     c("i", "TIBI")]
+  if (nrow(diag_df) < 1) next
+  
+
+  diag_df$poids <- Xi[as.character(diag_df$i)] / X_tot
+  diag_df$w <- diag_df$poids   #/ sum(diag_df$poids)
+  m <- sum(diag_df$w * diag_df$TIBI)
+  
+  resultats_AN <- rbind(resultats_AN, data.frame(
+    year = year, moyenne = m, n_blocs = nrow(diag_df)))
+}
+
+
+##############################################################
+#####  4. SUPERPOSITION DES TROIS COURBES  ###################
+##############################################################
+resultats_intra$methode   <- "Intramax"
+resultats_louvain$methode <- "Louvain"
+resultats_AN$methode      <- "Anderson-Norheim"
+
+combine <- rbind(resultats_intra[, c("year", "moyenne", "methode")],
+                 resultats_louvain[, c("year", "moyenne", "methode")],
+                 resultats_AN[, c("year", "moyenne", "methode")])
+
+p_combine <- ggplot(combine, aes(year, moyenne, color = methode)) +
+  geom_line() + geom_point(size = 1) +
+  geom_hline(yintercept = 0, linetype = 3, colour = "grey60") +
+  coord_cartesian(ylim = c(-1, 1)) +
+  scale_x_continuous(breaks = seq(1835, 1935, 5)) +
+  scale_color_manual(values = c("Intramax" = "#0055A4",
+                                "Louvain" = "#CF142B",
+                                "Anderson-Norheim" = "#2E8B57")) +
+  labs(x = "année", y = "TIBI intra-bloc moyen (pondéré)",
+       title = "Commerce intra-bloc moyen : Intramax vs Louvain vs Anderson-Norheim",
+       color = "Méthode") +
+  theme_minimal() +
+  theme(legend.position = "bottom",
+        axis.text.x = element_text(angle = 45, hjust = 1))
+
+print(p_combine)
+
+ggsave("data/blocks/tibi_comparaison_trois_methodes.png",
+       plot = p_combine, width = 10, height = 5, dpi = 150)
+
+
