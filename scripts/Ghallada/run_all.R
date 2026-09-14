d848e202195d24d285143c284efef1726121cfbf
YoussefGld <youssef.ghallada@uclouvain.be>
2026-09-14 21:29:24 +0100

    Sauvegarde des tables contig, mid et atop en csv

--- scripts/Ghallada/run_all.R
+++ scripts/Ghallada/run_all.R
@@ -14,3 +14,6 @@ source("scripts/Ghallada/Map year.R")
 setwd("/Users/youssef/Desktop/ricardo_gph_analysis")
 
 source("scripts/Ghallada/RegionalTradeintensityYear.R")
+
+setwd("/Users/youssef/Desktop/ricardo_gph_analysis")
+source("scripts/Ghallada/Merge in one file all.R")
